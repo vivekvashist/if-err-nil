f216087c52927ae979f98728bcd48fdcd1bae289
Vivek <vivekvashist@gmail.com>
2026-07-09 12:52:17 +0930

    more slices

--- slices/main.go
+++ slices/main.go
@@ -5,12 +5,17 @@ import (
 )
 
 func main() {
-	cart := []string{"apple", "banana", "milk"}
-	fmt.Printf("%q\n", cart)
-	fruit := cart[:2]
-	fmt.Printf("%q\n", fruit)
-	cart[0] = "pear"
-	fmt.Printf("%q\n", cart) // ["pear" "banana" "milk"]
-	fmt.Printf("%q\n", fruit) // ["pear" "banana"]
-
+	// cart := []string{"apple", "banana", "milk"}
+	// fmt.Printf("%q\n", cart)
+	// fruit := cart[:2]
+	// fmt.Printf("%q\n", fruit)
+	// cart[0] = "pear"
+	// fmt.Printf("%q\n", cart) // ["pear" "banana" "milk"]
+	// fmt.Printf("%q\n", fruit) // ["pear" "banana"]
+	a := []int{1, 2, 3}
+	b := a[:]
+	a = append(a, 4)
+	a[1] = 99
+	fmt.Printf("%v\n", a) // [1 99 3 4]
+	fmt.Printf("%v\n", b) // [1 2 3]
 }
