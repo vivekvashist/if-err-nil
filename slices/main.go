7143f97e94fbe61c7008ee8666cc70e78813b8a6
Vivek <vivekvashist@gmail.com>
2026-07-11 10:34:51 +0930

    slicing slices & append()

--- slices/main.go
+++ slices/main.go
@@ -19,18 +19,50 @@ func main() {
 	//	fmt.Printf("%v\n", a) // [1 99 3 4]
 	//	fmt.Printf("%v\n", b) // [1 2 3]
 
-	var intSlice []int = []int{3, 1, 4, 3, 9, 6}
-	stringSlice := []string{"The", "sky", "was", "the", "color"}
+	// var intSlice []int = []int{3, 1, 4, 3, 9, 6}
+	// stringSlice := []string{"The", "sky", "was", "the", "color"}
+	//
+	//	if n := len(intSlice); n > 0 {
+	//		fmt.Printf("intSlice has %d elements\n", n)
+	//	} else {
+	//
+	//		fmt.Printf("intSlice has no elements\n")
+	//	}
+	//
+	//	if n := len(stringSlice); n > 0 {
+	//		fmt.Printf("stringSlice has %d elements\n", n)
+	//	} else {
+	//
+	//		fmt.Printf("stringSlice has no elements\n")
+	//	}
 
-	if n := len(intSlice); n > 0 {
-		fmt.Printf("intSlice has %d elements\n", n)
-	} else {
-		fmt.Printf("intSlice has no elements\n")
-	}
+	nums := []int{1, 11, 21, 1211, 111221, 323311}
+	fmt.Printf("nums: %v\n", nums)
+	fmt.Printf("nums lenght: %d, nums capacity: %d\n", len(nums), cap(nums))
 
-	if n := len(stringSlice); n > 0 {
-		fmt.Printf("stringSlice has %d elements\n", n)
-	} else {
-		fmt.Printf("stringSlice has no elements\n")
-	}
+	middle := nums[1:3]
+	fmt.Printf("middle: %v\n", middle)
+	fmt.Printf("middle lenght: %d, middle capacity: %d\n", len(middle), cap(middle))
+
+	fmt.Printf("nums: %v\n", nums)
+	fmt.Printf("nums lenght: %d, nums capacity: %d\n", len(nums), cap(nums))
+	nums[1] *= 4
+
+	fmt.Printf("nums: %v\n", nums)
+	fmt.Printf("middle: %v\n", middle)
+	fmt.Printf("middle lenght: %d, middle capacity: %d\n", len(middle), cap(middle))
+
+	middle = append(middle, 100)
+	middle = append(middle, 200)
+	middle = append(middle, 300)
+	middle = append(middle, 400)
+
+	fmt.Printf("middle: %v\n", middle)
+	fmt.Printf("nums: %v\n", nums)
+	nums[1] *= 10
+
+	fmt.Printf("nums: %v\n", nums)
+	fmt.Printf("middle: %v\n", middle)
+	fmt.Printf("\n\n")
+
 }
