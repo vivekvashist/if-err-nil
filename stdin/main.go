d7bd2ee779b918c534dbb81f1790d36c2aa396fe
Vivek <vivekvashist@gmail.com>
2026-07-20 20:08:06 +0930

    bufio

---
+++ stdin/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		fmt.Printf("Echo: %s\n", scanner.Text())
+	}
+}
