5d967c905aa858a60defc06e1d5039f00545616b
Vivek <vivekvashist@gmail.com>
2026-08-15 23:47:05 +0930

    fmt verb 3x

--- verbIndexing/main.go
+++ verbIndexing/main.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Printf("normal order: %d, %s\n", 42, "hello")
 	fmt.Printf("reversed: %[2]s, %[1]d\n", 42, "hello")
 	fmt.Printf("hello %[1]s, hello again %[1]s\n", "gopher")
+	fmt.Printf("hello %[1]s, hello again %[1]s and again %[1]s\n", "gopher")
 }
